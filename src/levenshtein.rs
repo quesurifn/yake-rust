01f49b7bf54e3699bf1fe0543baaf8341e4d4ba2
quesurifn <kyle.c.r.fahey@gmail.com>
2023-02-11 18:46:44 -0500

    update distance

--- src/levenshtein.rs
+++ src/levenshtein.rs
@@ -7,9 +7,9 @@ impl Levenshtein {
     pub fn ratio(seq1: String, seq2: String) -> f64 {
         let mut distance = 0;
         if seq1.len() <= seq2.len() {           
-            let distance = levenshtein_distance(&seq1, &seq2);
+            distance = levenshtein_distance(&seq1, &seq2);
         } else {
-            let distance = levenshtein_distance(&seq2, &seq1);        
+            distance = levenshtein_distance(&seq2, &seq1);        
         }
         let length = max(seq1.len(), seq2.len());
         1.0 - (distance as f64 / length as f64)

ea36df4466571a3acb25e21d19134d07ad66a437
xamgore <xamgore@ya.ru>
2024-10-25 12:33:08 +0300

    Run tests on cargo test

--- yake_rust/src/lib.rs
+++ yake_rust/src/lib.rs
@@ -477,44 +477,49 @@ mod tests {
     type Results = Vec<ResultItem>;
     use crate::ResultItem;
 
+    #[test]
     fn keywords() {
         let text = r#"
-        Google is acquiring data science community Kaggle. Sources tell us that Google is acquiring Kaggle, a platform that hosts data science and machine learning 
-        competitions. Details about the transaction remain somewhat vague, but given that Google is hosting its Cloud 
-        Next conference in San Francisco this week, the official announcement could come as early as tomorrow. 
-        Reached by phone, Kaggle co-founder CEO Anthony Goldbloom declined to deny that the acquisition is happening. 
-        Google itself declined 'to comment on rumors'. Kaggle, which has about half a million data scientists on its platform, 
-        was founded by Goldbloom  and Ben Hamner in 2010. 
-        The service got an early start and even though it has a few competitors like DrivenData, TopCoder and HackerRank, 
-        it has managed to stay well ahead of them by focusing on its specific niche. 
-        The service is basically the de facto home for running data science and machine learning competitions. 
-        With Kaggle, Google is buying one of the largest and most active communities for data scientists - and with that, 
-        it will get increased mindshare in this community, too (though it already has plenty of that thanks to Tensorflow 
-        and other projects). Kaggle has a bit of a history with Google, too, but that's pretty recent. Earlier this month, 
-        Google and Kaggle teamed up to host a $100,000 machine learning competition around classifying YouTube videos. 
-        That competition had some deep integrations with the Google Cloud Platform, too. Our understanding is that Google 
-        will keep the service running - likely under its current name. While the acquisition is probably more about 
-        Kaggle's community than technology, Kaggle did build some interesting tools for hosting its competition 
-        and 'kernels', too. On Kaggle, kernels are basically the source code for analyzing data sets and developers can 
-        share this code on the platform (the company previously called them 'scripts'). 
-        Like similar competition-centric sites, Kaggle also runs a job board, too. It's unclear what Google will do with 
-        that part of the service. According to Crunchbase, Kaggle raised $12.5 million (though PitchBook says it's $12.75) 
+        Google is acquiring data science community Kaggle. Sources tell us that Google is acquiring Kaggle, a platform that hosts data science and machine learning
+        competitions. Details about the transaction remain somewhat vague, but given that Google is hosting its Cloud
+        Next conference in San Francisco this week, the official announcement could come as early as tomorrow.
+        Reached by phone, Kaggle co-founder CEO Anthony Goldbloom declined to deny that the acquisition is happening.
+        Google itself declined 'to comment on rumors'. Kaggle, which has about half a million data scientists on its platform,
+        was founded by Goldbloom  and Ben Hamner in 2010.
+        The service got an early start and even though it has a few competitors like DrivenData, TopCoder and HackerRank,
+        it has managed to stay well ahead of them by focusing on its specific niche.
+        The service is basically the de facto home for running data science and machine learning competitions.
+        With Kaggle, Google is buying one of the largest and most active communities for data scientists - and with that,
+        it will get increased mindshare in this community, too (though it already has plenty of that thanks to Tensorflow
+        and other projects). Kaggle has a bit of a history with Google, too, but that's pretty recent. Earlier this month,
+        Google and Kaggle teamed up to host a $100,000 machine learning competition around classifying YouTube videos.
+        That competition had some deep integrations with the Google Cloud Platform, too. Our understanding is that Google
+        will keep the service running - likely under its current name. While the acquisition is probably more about
+        Kaggle's community than technology, Kaggle did build some interesting tools for hosting its competition
+        and 'kernels', too. On Kaggle, kernels are basically the source code for analyzing data sets and developers can
+        share this code on the platform (the company previously called them 'scripts').
+        Like similar competition-centric sites, Kaggle also runs a job board, too. It's unclear what Google will do with
+        that part of the service. According to Crunchbase, Kaggle raised $12.5 million (though PitchBook says it's $12.75)
         since its   launch in 2010. Investors in Kaggle include Index Ventures, SV Angel, Max Levchin, Naval Ravikant,
-        Google chief economist Hal Varian, Khosla Ventures and Yuri Milner 
+        Google chief economist Hal Varian, Khosla Ventures and Yuri Milner
         "#;
 
-        let kwds = super::Yake::new(None, None).get_n_best(text.to_string(), Some(10));
+        let mut kwds = super::Yake::new(None, None).get_n_best(text.to_string(), Some(10));
+
+        // leave only 4 digits
+        kwds.iter_mut().for_each(|r| r.score = (r.score * 10_000.).trunc() / 10_000.);
+
         let results: Results = vec![
-            ResultItem { raw: "Kaggle".to_owned(), keyword: "kaggle".to_owned(), score: 0.20846279315962324 },
-            ResultItem { raw: "Google".to_owned(), keyword: "google".to_owned(), score: 0.23676437642810488 },
-            ResultItem { raw: "acquiring Kaggle".to_owned(), keyword: "acquiring kaggle".to_owned(), score: 0.3017882425537463 },
-            ResultItem { raw: "data science".to_owned(), keyword: "data science".to_owned(), score: 0.30873986543219967 },
-            ResultItem { raw: "Google Cloud".to_owned(), keyword: "google cloud".to_owned(), score: 0.40955463454967833 },
-            ResultItem { raw: "Google Cloud Platform".to_owned(), keyword: "google cloud platform".to_owned(), score: 0.5018536215405839 },
-            ResultItem { raw: "acquiring data science".to_owned(), keyword: "acquiring data science".to_owned(), score: 0.5494143207629893 },
-            ResultItem { raw: "San Francisco".to_owned(), keyword: "san francisco".to_owned(), score: 0.7636151899513093 },
-            ResultItem { raw: "CEO Anthony Goldbloom".to_owned(), keyword: "ceo anthony goldbloom".to_owned(), score: 0.8166005339007906 },
-            ResultItem { raw: "science community Kaggle".to_owned(), keyword: "science community kaggle".to_owned(), score: 0.8690005548383123 },
+            ResultItem { raw: "Kaggle".to_owned(), keyword: "kaggle".to_owned(), score: 0.2084 },
+            ResultItem { raw: "Google".to_owned(), keyword: "google".to_owned(), score: 0.2367 },
+            ResultItem { raw: "acquiring Kaggle".to_owned(), keyword: "acquiring kaggle".to_owned(), score: 0.3017 },
+            ResultItem { raw: "data science".to_owned(), keyword: "data science".to_owned(), score: 0.3087 },
+            ResultItem { raw: "Google Cloud".to_owned(), keyword: "google cloud".to_owned(), score: 0.4095 },
+            ResultItem { raw: "Google Cloud Platform".to_owned(), keyword: "google cloud platform".to_owned(), score: 0.5018 },
+            ResultItem { raw: "acquiring data science".to_owned(), keyword: "acquiring data science".to_owned(), score: 0.5494 },
+            ResultItem { raw: "San Francisco".to_owned(), keyword: "san francisco".to_owned(), score: 0.7636 },
+            ResultItem { raw: "CEO Anthony Goldbloom".to_owned(), keyword: "ceo anthony goldbloom".to_owned(), score: 0.8166 },
+            ResultItem { raw: "science community Kaggle".to_owned(), keyword: "science community kaggle".to_owned(), score: 0.8690 },
         ];
 
         assert_eq!(kwds, results);

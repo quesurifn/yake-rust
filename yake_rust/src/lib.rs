0d1dfbd13b205e2a19e8781773f991daa281e196
bunny-therapist <anton.vikstroem@gmail.com>
2024-10-29 16:46:54 +0300

    Add parenthesis to score expression

--- yake_rust/src/lib.rs
+++ yake_rust/src/lib.rs
@@ -383,7 +383,7 @@ impl Yake {
                 }
 
                 let tf = v.surface_forms.len() as f64;
-                let weight = prod_ / tf * (1.0 + sum_);
+                let weight = prod_ / (tf * (1.0 + sum_));
 
                 final_weights.insert(candidate.clone(), weight);
                 surface_to_lexical.insert(candidate.clone(), v.lexical_form.join(" "));
@@ -497,31 +497,35 @@ mod tests {
         kwds.iter_mut().for_each(|r| r.score = (r.score * 10_000.).trunc() / 10_000.);
 
         let results: Results = vec![
-            ResultItem { raw: "Kaggle".to_owned(), keyword: "kaggle".to_owned(), score: 0.2084 },
-            ResultItem { raw: "Google".to_owned(), keyword: "google".to_owned(), score: 0.2367 },
-            ResultItem { raw: "acquiring Kaggle".to_owned(), keyword: "acquiring kaggle".to_owned(), score: 0.3017 },
-            ResultItem { raw: "data science".to_owned(), keyword: "data science".to_owned(), score: 0.3087 },
-            ResultItem { raw: "Google Cloud".to_owned(), keyword: "google cloud".to_owned(), score: 0.4095 },
+            ResultItem { raw: "data science".to_owned(), keyword: "data science".to_owned(), score: 0.0582 },
             ResultItem {
                 raw: "Google Cloud Platform".to_owned(),
                 keyword: "google cloud platform".to_owned(),
-                score: 0.5018,
+                score: 0.0631,
             },
             ResultItem {
                 raw: "acquiring data science".to_owned(),
                 keyword: "acquiring data science".to_owned(),
-                score: 0.5494,
-            },
-            ResultItem { raw: "San Francisco".to_owned(), keyword: "san francisco".to_owned(), score: 0.7636 },
-            ResultItem {
-                raw: "CEO Anthony Goldbloom".to_owned(),
-                keyword: "ceo anthony goldbloom".to_owned(),
-                score: 0.8166,
+                score: 0.0704,
             },
             ResultItem {
                 raw: "science community Kaggle".to_owned(),
                 keyword: "science community kaggle".to_owned(),
-                score: 0.8690,
+                score: 0.0768,
+            },
+            ResultItem { raw: "acquiring Kaggle".to_owned(), keyword: "acquiring kaggle".to_owned(), score: 0.0904 },
+            ResultItem {
+                raw: "CEO Anthony Goldbloom".to_owned(),
+                keyword: "ceo anthony goldbloom".to_owned(),
+                score: 0.0924,
+            },
+            ResultItem { raw: "Google Cloud".to_owned(), keyword: "google cloud".to_owned(), score: 0.1061 },
+            ResultItem { raw: "Kaggle".to_owned(), keyword: "kaggle".to_owned(), score: 0.1165 },
+            ResultItem { raw: "Google".to_owned(), keyword: "google".to_owned(), score: 0.1344 },
+            ResultItem {
+                raw: "Anthony Goldbloom declined".to_owned(),
+                keyword: "anthony goldbloom declined".to_owned(),
+                score: 0.1447,
             },
         ];
 

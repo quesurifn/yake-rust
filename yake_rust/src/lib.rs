e1da6efa0f05cd95494e0beb8eeb0b97d7a99501
bunny-therapist <anton.vikstroem@gmail.com>
2024-10-29 16:53:01 +0300

    Fix casing logarithm (fix #11)

--- yake_rust/src/lib.rs
+++ yake_rust/src/lib.rs
@@ -287,7 +287,7 @@ impl Yake {
             }
 
             cand.casing = cand.tf_a.max(cand.tf_u);
-            cand.casing /= 1.0 + cand.tf.ln_1p();
+            cand.casing /= 1.0 + cand.tf.ln();
 
             let sentence_ids = word.iter().map(|o| o.idx).collect::<HashSet<usize>>();
             cand.position = (3.0 + median(sentence_ids.iter().copied()).unwrap()).ln();
@@ -497,24 +497,36 @@ mod tests {
         kwds.iter_mut().for_each(|r| r.score = (r.score * 10_000.).trunc() / 10_000.);
 
         let results: Results = vec![
-            ResultItem { raw: "data science".into(), keyword: "data science".into(), score: 0.0599 },
-            ResultItem { raw: "Google Cloud Platform".into(), keyword: "google cloud platform".into(), score: 0.0656 },
             ResultItem {
-                raw: "acquiring data science".into(),
-                keyword: "acquiring data science".into(),
+                raw: "CEO Anthony Goldbloom".to_owned(),
+                keyword: "ceo anthony goldbloom".to_owned(),
+                score: 0.0463,
+            },
+            ResultItem {
+                raw: "Google Cloud Platform".to_owned(),
+                keyword: "google cloud platform".to_owned(),
+                score: 0.0566,
+            },
+            ResultItem { raw: "data science".to_owned(), keyword: "data science".to_owned(), score: 0.0599 },
+            ResultItem {
+                raw: "acquiring data science".to_owned(),
+                keyword: "acquiring data science".to_owned(),
                 score: 0.0735,
             },
             ResultItem {
-                raw: "science community Kaggle".into(),
-                keyword: "science community kaggle".into(),
-                score: 0.0804,
+                raw: "science community Kaggle".to_owned(),
+                keyword: "science community kaggle".to_owned(),
+                score: 0.0795,
             },
-            ResultItem { raw: "acquiring Kaggle".into(), keyword: "acquiring kaggle".into(), score: 0.0924 },
-            ResultItem { raw: "CEO Anthony Goldbloom".into(), keyword: "ceo anthony goldbloom".into(), score: 0.096 },
-            ResultItem { raw: "Google Cloud".into(), keyword: "google cloud".into(), score: 0.1085 },
-            ResultItem { raw: "Kaggle".into(), keyword: "kaggle".into(), score: 0.1178 },
-            ResultItem { raw: "Google".into(), keyword: "google".into(), score: 0.1357 },
-            ResultItem { raw: "machine learning".into(), keyword: "machine learning".into(), score: 0.1513 },
+            ResultItem { raw: "San Francisco".to_owned(), keyword: "san francisco".to_owned(), score: 0.0909 },
+            ResultItem { raw: "acquiring Kaggle".to_owned(), keyword: "acquiring kaggle".to_owned(), score: 0.0915 },
+            ResultItem {
+                raw: "Anthony Goldbloom declined".to_owned(),
+                keyword: "anthony goldbloom declined".to_owned(),
+                score: 0.0964,
+            },
+            ResultItem { raw: "Google Cloud".to_owned(), keyword: "google cloud".to_owned(), score: 0.0969 },
+            ResultItem { raw: "Kaggle".to_owned(), keyword: "kaggle".to_owned(), score: 0.1167 },
         ];
 
         assert_eq!(kwds, results);
